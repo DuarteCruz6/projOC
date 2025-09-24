227c81e4e9c970b61fdada95a44f4c205d299fd0
duarte cruz <dudu.cruz2005@gmail.com>
2025-09-24 12:02:36 +0100

    bug mental

--- proj1/medicoes/ex2/to_plot_all_data.py
+++ proj1/medicoes/ex2/to_plot_all_data.py
@@ -1,8 +1,7 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 excel_path = os.path.join(script_dir, "all_data.xlsx")
@@ -11,40 +10,30 @@ df = pd.read_excel(excel_path)
 
 df["array size"] = pd.to_numeric(df["array size"])
 df["stride"] = pd.to_numeric(df["stride"])
-df["# mean access time (nanosec)"] = pd.to_numeric(df["# mean access time (nanosec)"])
+df["# mean access time (nanosec)"] = pd.to_numeric(df["# mean access time (nanosec)"])     
 
 plt.figure(figsize=(12, 6))
 
-unique_sizes = df["array size"].unique()
-cmap = plt.colormaps.get_cmap("tab20")   # só o nome
-colors = [cmap(i) for i in np.linspace(0, 1, len(unique_sizes))]
-
-for i,arraySize in enumerate(unique_sizes):
-    subset = df[df["array size"] == arraySize]
-    
+for stride in df["stride"].unique():
+    subset = df[df["stride"] == stride]
     plt.plot(
-        subset["stride"], 
+        subset["array size"],
         subset["# mean access time (nanosec)"],
-        marker='o', 
-        color=colors[i], 
-        label=f"Array size {arraySize}"
+        marker='o',
+        label=f"stride {stride}",
     )
-
+    
 plt.title("# mean access time (nanosec) vs Stride")
-plt.xlabel("Stride")
+plt.xlabel("Array size")
 plt.ylabel("# mean access time (nanosec)")
 
-# Customize X-axis: 2^0 → 2^20 with steps 2^x
-x_ticks = [2**i for i in range(21)]
+x_ticks = [2**i for i in range(12,24)]
 plt.xscale("log", base=2)
-plt.xticks(x_ticks, [f"2^{i}" for i in range(21)])
-
+plt.xticks(x_ticks, [f"{2**i}" for i in range(12,24)])
 plt.grid(True, which="both", linestyle="--", linewidth=0.5)
 plt.legend()
 
 plot_path = os.path.join(script_dir, "plot-allstrides.png")
 plt.savefig(plot_path)
 plt.show()
-
 print(f"Plot saved as: {plot_path}")
-
