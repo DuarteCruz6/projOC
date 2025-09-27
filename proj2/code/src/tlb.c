8f1a6e81c12bd40704464360f39376514a3b695f
Daniel Martins Borges <daniel.m.borges@tecnico.ulisboa.pt>
2025-09-27 14:23:49 +0100

    Proj2 cache l1 (por testar)

--- proj2/code/src/tlb.c
+++ proj2/code/src/tlb.c
@@ -10,6 +10,9 @@
 #include "memory.h"
 #include "page_table.h"
 
+#define DRAM_PAGES (uint64_t)(1llu << (DRAM_ADDRESS_BITS - PAGE_SIZE_BITS))
+#define PHYSICAL_PAGE_NUMBER_MASK (DRAM_PAGES - 1)
+
 typedef struct {
   bool valid;
   bool dirty;
@@ -49,13 +52,97 @@ void tlb_init() {
 }
 
 void tlb_invalidate(va_t virtual_page_number) {
-  (void)(virtual_page_number);  // Suppress unused variable warning. You can
-                                // delete this when implementing the actual
-                                // function.
-  // TODO: implement TLB entry invalidation.
+
+  increment_time(TLB_L1_LATENCY_NS);
+
+  // search for the entry with the correct virtual page number
+  for (size_t i = 0; i < TLB_L1_SIZE; i++)
+  {
+    if (tlb_l1[i].valid && tlb_l1[i].virtual_page_number == virtual_page_number) {
+
+      tlb_l1[i].valid = false;
+      tlb_l1_invalidations++;
+      
+      // i don't know if this check is needed, this function is only called when the entry is dirty, but it shouldn't be necessary
+      if (tlb_l1[i].dirty) {
+        write_back_tlb_entry(virtual_page_number);
+      }
+      
+      log_dbg("Invalidated page %" PRIu64 "on Cache L1.", virtual_page_number);
+      return;
+    }
+  }
+  
+}
+
+void set_tlb_entry(tlb_entry_t* entry, va_t virtual_page_number, pa_dram_t physical_page_number, uint64_t last_access) {
+  entry -> valid = true;
+  entry -> dirty = false;
+  entry -> last_access = last_access;
+  entry -> virtual_page_number = virtual_page_number;
+  entry -> physical_page_number = physical_page_number;
 }
 
 pa_dram_t tlb_translate(va_t virtual_address, op_t op) {
-  // TODO: implement the TLB logic.
-  return page_table_translate(virtual_address, op);
+
+  virtual_address &= VIRTUAL_ADDRESS_MASK;
+  va_t virtual_page_offset = virtual_address & PAGE_OFFSET_MASK;
+  va_t virtual_page_number = (virtual_address >> PAGE_SIZE_BITS) & PAGE_INDEX_MASK;
+
+  // Check in Cache L1
+  increment_time(TLB_L1_LATENCY_NS);
+  tlb_entry_t* tlb_l1_empty_entry = NULL;
+  tlb_entry_t* tlb_l1_last_used_entry = &tlb_l1[0];
+
+  for (size_t i = 0; i < TLB_L1_SIZE; i++)
+  {
+    // Found page in TLB
+    if (tlb_l1[i].valid && tlb_l1[i].virtual_page_number == virtual_page_number) {
+
+      tlb_l1_hits++;
+      tlb_l1[i].last_access = tlb_l1_hits + tlb_l1_misses;
+
+      if (op == OP_WRITE) {
+        tlb_l1[i].dirty = true;
+      }
+
+      pa_dram_t translated_address = (tlb_l1[i].physical_page_number << PAGE_SIZE_BITS) | virtual_page_offset;
+      log_dbg("Cache L1 found (VA=%" PRIx64 " VPN=%" PRIx64 " PA=%" PRIx64 ")",
+              virtual_address, virtual_page_number, translated_address);
+
+      return translated_address;
+    }
+    // Get empty entry
+    else if (!tlb_l1[i].valid && !tlb_l1_empty_entry) {
+      tlb_l1_empty_entry = &tlb_l1[i];
+    }
+    // Get oldest access entry
+    else if (tlb_l1[i].valid && tlb_l1[i].last_access < tlb_l1_last_used_entry -> last_access) {
+      tlb_l1_last_used_entry = &tlb_l1[i];
+    }
+  }
+  tlb_l1_misses++;
+
+  // Check in Cache L2
+  //
+  // TODO
+
+  pa_dram_t physical_add = page_table_translate(virtual_address, op);
+  pa_dram_t physical_page_number = (physical_add >> PAGE_SIZE_BITS) & PHYSICAL_PAGE_NUMBER_MASK;
+
+  if (tlb_l1_empty_entry) {
+    set_tlb_entry(tlb_l1_empty_entry, virtual_page_number, physical_page_number, tlb_l1_hits + tlb_l1_misses);
+  }
+  else {
+    tlb_l1_invalidations++;
+
+    if (tlb_l1_last_used_entry -> dirty) {
+      va_t replaced_virtual_address = (tlb_l1_last_used_entry -> virtual_page_number << PAGE_SIZE_BITS) & VIRTUAL_ADDRESS_MASK;
+      write_back_tlb_entry(replaced_virtual_address);
+    }
+
+    set_tlb_entry(tlb_l1_last_used_entry, virtual_page_number, physical_page_number, tlb_l1_hits + tlb_l1_misses);
+  }
+
+  return physical_add;
 }
